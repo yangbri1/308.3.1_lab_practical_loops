591a053abd470652cc78a82be9eea68e5e0150bd
brian <yangbri1@msu.edu>
2024-09-10 15:41:37 -0400

    trying to get the next prime number after recognizing the first -- think I'm overthinking

--- script.js
+++ script.js
@@ -15,31 +15,31 @@ console.log(`/* ------------------ Part 1: Fizz Buzz ------------------ */`);
     If a number is not divisible by either 3 or 5, log the number.*/
 
 // iterate through the for loop 100 times ...
-for(let i = 1; i < 101; i++){
+// for(let i = 1; i < 101; i++){
 
-    // "most specific to most ambiguous"
-    // if the iterator is divisible by 5 AND 3
-    if(i % 5 === 0 && i % 3 === 0){
-        // log out "Fizz Buzz"
-        console.log(`${i}: Fizz Buzz`);
-    }
-    // if the iterator is only divisible by 3
-    else if(i % 3 === 0){
-        // log out "Fizz"
-        console.log(`${i}: Fizz`);
-    }
-    // if the iterator is only divisible by 5
-    else if(i % 5 === 0){
-        // log out "Buzz"
-        console.log(`${i}: Buzz`);
-    }
-    // otherwise if the iterator is does NOT fall into any of the cases above (NOT divisible by 3, 5, or both)
-    else{
-        // log out the number itself
-        console.log(`${i}: ${i}`);
-    }
+//     // "most specific to most ambiguous"
+//     // if the iterator is divisible by 5 AND 3
+//     if(i % 5 === 0 && i % 3 === 0){
+//         // log out "Fizz Buzz"
+//         console.log(`${i}: Fizz Buzz`);
+//     }
+//     // if the iterator is only divisible by 3
+//     else if(i % 3 === 0){
+//         // log out "Fizz"
+//         console.log(`${i}: Fizz`);
+//     }
+//     // if the iterator is only divisible by 5
+//     else if(i % 5 === 0){
+//         // log out "Buzz"
+//         console.log(`${i}: Buzz`);
+//     }
+//     // otherwise if the iterator is does NOT fall into any of the cases above (NOT divisible by 3, 5, or both)
+//     else{
+//         // log out the number itself
+//         console.log(`${i}: ${i}`);
+//     }
 
-} // end of Fizz Buzz for-loop block
+// } // end of Fizz Buzz for-loop block
 
 
 /* ------------------ Part 2: Prime Time ------------------ */
@@ -48,4 +48,29 @@ console.log(`/* ------------------ Part 2: Prime Time ------------------ */`);
 /* Objective: Write a script that accomplishes the following:
     Declare an arbitrary number, n.
     Create a loop that searches for the next prime number, starting at n and incrementing from there.
-    As soon as you find the prime number, log that number and exit the loop.*/
+    As soon as you find the prime number, log that number and exit the loop.
+    
+    Continuing with the example above, 
+    if n is equal to 4, your loop should log 5. 
+    Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. 
+    Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
+    Be careful! If you set n to a number too large, your loop could take a long time to process.*/
+
+let n = 7;
+
+// let j = 0;
+let nplus = n + 1;
+do{
+    if(n % 2 != 0 && n % 3 !== 0){
+        console.log(`${n} is prime`);
+        break;
+    }
+    n++;
+}
+while(n > 0){
+    if(nplus % 2 != 0 && nplus % 3 !== 0){
+        console.log(`${nplus} is prime`);
+        
+    }
+    nplus++;
+}
